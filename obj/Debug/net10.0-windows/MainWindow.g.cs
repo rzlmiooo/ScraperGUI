24c3cb24a02b286b0f7ddfb78c2e569a2a5bd821
rzlmiooo <zrrizall10@example.com>
2026-05-22 10:12:06 +0700

    jos

--- obj/Debug/net10.0-windows/MainWindow.g.cs
+++ obj/Debug/net10.0-windows/MainWindow.g.cs
@@ -83,7 +83,7 @@ namespace ScraperGUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ScraperGUI;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ScraperGUI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
